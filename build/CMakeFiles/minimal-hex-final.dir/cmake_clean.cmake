30d07c3c1f07ad8c6ab91fb98a6935bf85f3a631
DanielSacro <sacro@ualberta.ca>
2024-03-03 20:31:52 -0700

    Remove build directory and add build/ to .gitignore

--- build/CMakeFiles/minimal-hex-final.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/minimal-hex-final"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/minimal-hex-final.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/minimal-opt.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/minimal-opt"
-  "minimal.opt"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/minimal-opt.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
