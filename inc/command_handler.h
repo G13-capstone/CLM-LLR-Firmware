9922ddece341a50399d4a92b4f3d8fde3c5757d9
DanielSacro <sacro@ualberta.ca>
2024-03-06 23:53:22 -0700

    Add code for parsing commands and forcing them to lowercase

--- inc/command_handler.h
+++ inc/command_handler.h
@@ -1,11 +1,15 @@
 #ifndef __COMMAND_HANDLER_H__
 #define __COMMAND_HANDLER_H__
 
+#define MAX_COMMAND_LENGTH 50 // in characters
+#define MAX_INPUT_TOKENS 10
+
 int print_test(void);
 
 class CommandHandler {
     private:
         bool CLI_printed = false;
+        bool input_entered = true; // flag used for dev/debug
         void print_CLI(void);
     public:
         void handle_command(void);
--- src/command_handler.cpp
+++ src/command_handler.cpp
@@ -1,7 +1,8 @@
 // #include <SystemClock.h>
 #include <XPD.h>
 // #include <GPIO.h>
-#include <string>
+#include <string.h> // for strtok
+#include <cctype> // For tolower()
 using namespace std;
 
 // #include "main.h"
@@ -17,7 +18,7 @@ int print_test(void) {
 // Prints "UAOS CLI: " if it wasn't printed before
 void CommandHandler::print_CLI(void) {
     if (!CLI_printed) {
-        xpd_puts("UAOS CLI: ");
+        xpd_puts("UAOS CLI: ScRatCh test.py");
         CLI_printed = true;
     }
 }
@@ -28,21 +29,50 @@ void CommandHandler::handle_command(void) {
     print_CLI();
 
     // Check terminal for entered command
-    
-        // If there's an entered command, handle it
+    if (input_entered) {
+        // If there's an entered command, then handle it
+        // Get command (hardcode command for now)
+        char input[MAX_COMMAND_LENGTH] = "ScRatCh test.py";
+        
+        // Parse commands into tokens (for arguments)
+        char * inputToken = strtok(input, " ");
+        char * inputTokens[MAX_INPUT_TOKENS];
+        int i = 0;
+        while (inputToken != NULL) {
+            inputTokens[i] = inputToken;
+            inputToken = strtok(NULL, " ");
+            i++;
+        }
 
-        // Get command
+        // Debug, may or may not print properly
+        xpd_puts("Input read: ");
+        xpd_puts(inputTokens[0]);
+        xpd_puts(inputTokens[1]);
+        xpd_puts("\n");
 
-        // Pre-process and parse commands into tokens (for arguments)
+        // Force actual command to lowercase (not arguments)
+        char *commandName = inputTokens[0];
+        for (int j = 0; j < strlen(commandName); j++) {
+            commandName[j] = tolower(commandName[j]);
+        }
 
+        // Debug, may or may not print properly
+        xpd_puts("Input forced to lowercase: ");
+        xpd_puts(inputTokens[0]);
+        xpd_puts(inputTokens[1]);
+        xpd_puts("\n");
+        
         // Validate command by trying to find it in the command library
 
             // If function found, execute it
 
                 // If there's an error during execution, print error
 
-            // If funciton not found, print not found
+            // If function not found, print not found
 
-        // If there's no entered command, do nothing
+    } 
+    // If there's no entered command, do nothing
 
+    // Debug. Run handle_command only once
+    input_entered = false;
 }
