2c323a1a7fde20c9c5200c79f0996018aba48642
Nicholas Chin <nic.c3.14@gmail.com>
2024-03-23 14:45:36 -0600

    src/main.cpp: Add LCDDriver as another output

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 
 #include "main.h"
 #include "uart_io.h"
+#include "LCD.h"
+
 
 // sys_clock_wait can only wait a maximum of 65535 ticks
 // use a loop to get a longer delay.
@@ -30,6 +32,9 @@ char getc(IO &io) {
   return io.read_byte();
 }
 
+using namespace LCD_Functions;
+using L = LCD_STARTUP<DisplaySettings::DISPLAY_ON, CursorSettings::CURSOR_ON, CursorBlink::BLINK_OFF, Cursor_Line::LCD_HOME_L1>;
+
 // main() runs in thread 0
 int main(void)
 {
@@ -38,6 +43,21 @@ int main(void)
   //uint16_t count = 0;
   Uart_IO uart_io_driver = Uart_IO::Uart_IO(GPIO_C, 6, GPIO_C, 7, Uart_IO::BAUD_9600);
   puts(uart_io_driver, "Hello\r\n");
+  L lcd_io_driver;
+
+  SPI_set_config((SPI_ENABLE | SPI_MASTER | SPI_CLK_RATE_DIV_256) & ~(SPI_CLK_PHASE | SPI_CLK_IDLE_AT_1), SPI0);
+  gpio_set_config(gpio_get_config(GPIO_C) | (0x0C << 8), GPIO_C); // PC3 will be used as the Reset Pin, PC2 is Chip Select
+  ResetLCD();
+  // Set CS and reset high
+  gpio_write(gpio_get_output_reg(GPIO_C) | 0xC, GPIO_C);
+  lcd_io_driver.initializeDOGM204();
+  lcd_io_driver.setViewAngleTop();
+  lcd_io_driver.clrDisplay();
+  lcd_io_driver.setDisplayMode();
+  lcd_io_driver.setCursorMode();
+  lcd_io_driver.setBlinkMode();
+  lcd_io_driver.setCursor(0, 0);
+
   while (true) {
     char c =  getc(uart_io_driver);
     if ('a' <= c && c <= 'z') {
@@ -46,7 +66,7 @@ int main(void)
       c = 'a' + c - 'A';
     }
     putc(uart_io_driver, c);
-    puts(uart_io_driver, "\r\n");
+    putc(lcd_io_driver, c);
   }
 
   return 0;
