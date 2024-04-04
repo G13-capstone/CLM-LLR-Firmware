b61cb846c48b303914addf157e96e9d32517cdcd
Norman Zhang <norman.x.zhang@gmail.com>
2024-04-04 01:58:52 -0600

    if statement implementation

--- src/command_library.cpp
+++ src/command_library.cpp
@@ -2,20 +2,43 @@
 
 #include command_library.h
 
-void search_command(const char* command, char *args[]) {
-    switch(command) {
+bool compare_string(char *str1, char *str2) {
+    int i = 0;
+    while (true) {
+        if (str1[i] == '\0' && str2[i] == '\0') {
+            return true;
+        } else if (str1[i] != str2[i]) {
+            return false;
+        }
+        i++;
+    }
+}
 
-        case "echo":
-            echo_command(args);
-            break;
-        
-        case "save":
-            save_command(args);
-            break;
-        
-        default:
-            invalid_command();
-            break;
+void search_command(const char* command, char *args[]) {
+
+    if (compare_string(command, "echo")) {
+        echo_command(args);
+    }
+
+    else if (compare_string(command, "save")) {
+        save_command();
+    }
+
+    else if (compare_string(command, "load")) {
+        load_command();
+    }
+
+    else if (compare_string(command, "copy")) {
+        copy_command();
+    }
+
+    else if (compare_string(command, "scratch")) {
+        scratch_command();
+    }
+
+    // Invalid Command
+    else {
+        invalid_command();
     }
 }
 
@@ -25,6 +48,18 @@ void echo_command(char *args[]) {
 
 void save_command() {
     puts("Save command\n");
+}
+
+void load_command() {
+    puts("Load command\n");
+}
+
+void copy_command() {
+    puts("Copy command\n");
+}
+
+void scratch_command() {
+    puts("Scratch command\n");
 }
 
 void invalid_command() {
